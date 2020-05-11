aa5e068cc48e8ee8e6555d271f6c7faf35c468f5
Teresa Cho <tcho@cca.edu>
2020-05-10 23:28:54 -0700

    removes strictMode React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <App />,
+
   document.getElementById('root')
 );
