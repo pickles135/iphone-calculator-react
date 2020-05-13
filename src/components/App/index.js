ba520629384c46d4d52412128eea31757d1c6906
Teresa Cho <tcho@cca.edu>
2020-05-13 00:05:37 -0700

    creates div w/ className of top for the time

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="App">Hello from App</div>
+  return <div className="App">
+    <div className="Top">4:43</div>
+  </div>
 };
 
 export default App;
