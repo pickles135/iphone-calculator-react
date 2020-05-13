8042c4bf03c329b9a941bcb7307e7126598bdb5c
Teresa Cho <tcho@cca.edu>
2020-05-13 00:09:14 -0700

    creates another div for the display of numbers inputtted

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const App = () => {
   return <div className="App">
-    <div className="Top">4:43</div>
+    <div className="top">4:43</div>
+    <div className="display">0</div>
   </div>
 };
 
