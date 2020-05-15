b16439c9364a0097a94d6aaa14dbe507a5f00813
Teresa Cho <tcho@cca.edu>
2020-05-14 19:47:44 -0700

    imports App into index.js and also fixes the return statement in components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="App">
+  return( 
+  <div className="App">
     <div className="top">4:43</div>
     <div className="display">0</div>
     <div className="buttons">AC</div>
+    <div className="bottom">-</div>
   </div>
+  );
 };
 
 export default App;
