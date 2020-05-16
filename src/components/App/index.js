be55896952a261dbdfee988d2d3e10654782861e
Teresa Cho <tcho@cca.edu>
2020-05-16 11:16:45 -0700

    imports button into app, index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.css';
+// import Button from '../Button';
 
 const App = () => {
   return( 
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,3 +2,8 @@ import React from 'react';
 
  import './Button.css';
   
+ const Button = (props) => {
+   return <div className='Button'>AC</div>
+ }
+
+ export default Button;
