bcc9ec2e07d861a169369cbcb5248987f7f5948a
Teresa Cho <tcho@cca.edu>
2020-05-16 11:52:16 -0700

    passes the content over to the Button component in index.js in App folder

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,7 +9,7 @@ const App = () => {
     <div className="top">4:43</div>
     <div className="display">0</div>
     <div className="buttons">
-      <Button />
+      <Button content = 'AC'/>
     </div>
     <div className="bottom">-</div>
   </div>
