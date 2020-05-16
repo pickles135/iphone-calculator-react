234187bf7d758e3e66579a350b9517a0529ce01b
Teresa Cho <tcho@cca.edu>
2020-05-16 11:20:26 -0700

    successfully imports the button component into App - index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import './App.css';
-// import Button from '../Button';
+import Button from '../Button';
 
 const App = () => {
   return( 
   <div className="App">
     <div className="top">4:43</div>
     <div className="display">0</div>
-    <div className="buttons">AC</div>
+    <div className="buttons">
+      <Button />
+    </div>
     <div className="bottom">-</div>
   </div>
   );
